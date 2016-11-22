765cd773cf52777eb817300414004bb9524d22c7
Joe Niland <joe@originalmind.com.au>
2016-11-22 11:51:26 +1100

    Fix file contents replace for PS2; move fixed values to variables

--- tools/ChocolateyInstall.ps1
+++ tools/ChocolateyInstall.ps1
@@ -1,22 +1,32 @@
 try {
 
+   Set-Variable -Name DB_DIR -Value d:\redis-db\ -Option Constant
+   Set-Variable -Name CONF_DIR -Value d:\redis-conf\ -Option Constant
+   Set-Variable -Name CONF_FILE -Value redis.windows-service.conf -Option Constant
+
+   $redisConfig = (Join-Path $CONF_DIR $CONF_FILE)
+
    choco install redis-64 -y
 
-   if (!(Test-Path d:\redis-conf)) { mkdir d:\redis-conf }
+   if (!(Test-Path $CONF_DIR)) { mkdir $CONF_DIR }
    
-   if (!(Test-Path d:\redis-db)) { mkdir d:\redis-db }
+   if (!(Test-Path $DB_DIR)) { mkdir $DB_DIR }
    
    # Copy default config to custom config location
-   copy C:\ProgramData\chocolatey\lib\redis-64\redis.windows-service.conf d:\redis-conf\
+   copy C:\ProgramData\chocolatey\lib\redis-64\redis.windows-service.conf $CONF_DIR
 
    # Configure log file
    # Doesn't work in PowerShell 2
-   (Get-Content d:\redis-conf\redis.windows-service.conf).replace('logfile "Logs/redis_log.txt"', '# logfile "Logs/redis_log.txt"') | Set-Content d:\redis-conf\redis.windows-service.conf
+   if ($PSVersionTable.PSVersion.Major -le 2) {
+      $content = Get-Content -Path $redisConfig | Out-String
+   } else {
+      $content = Get-Content $redisConfig
+   }
 
-   # Bind locally only
-   (Get-Content d:\redis-conf\redis.windows-service.conf).replace('# bind 127.0.0.1', 'bind 127.0.0.1') | Set-Content d:\redis-conf\redis.windows-service.conf
+   $content = $content.replace('logfile "Logs/redis_log.txt"', '# logfile "Logs/redis_log.txt"')
+   $content.replace('# bind 127.0.0.1', 'bind 127.0.0.1') | Set-Content $redisConfig
 
-   redis-server --service-install d:\redis-conf\redis.windows-service.conf --loglevel verbose
+   redis-server --service-install $redisConfig --loglevel verbose
    redis-server --service-start
 
 } catch {
